425ff3760dd6b7ac54eb036cb932f10f79226720
sshwy <jy.cat@qq.com>
2021-01-22 10:16:35 +0800

    refactor(scripts/post_hash): refactor the code

--- scripts/post_hash.js
+++ scripts/post_hash.js
@@ -3,34 +3,37 @@
 const jsYaml = require('js-yaml');
 const crypto = require('crypto');
 const fs = require('fs');
-const momentTZ = require('moment-timezone');
 const moment = require('moment');
+const log = require('hexo-log')({
+  debug: false,
+  silent: false
+});
 
+function parseFrontMatter(raw){
+  const frontMatterMatch = raw.match(/^---\n(.*?)\n---\n/s);
+  if (frontMatterMatch === null) return null;
+  return jsYaml.load(frontMatterMatch[1] || '', { schema: jsYaml.JSON_SCHEMA });
+}
+function shaHash(raw){
+  let shasum = crypto.createHash('sha1');
+  shasum.update(raw);
+  return shasum.digest('hex');
+}
 hexo.extend.filter.register('before_post_render', function (data) {
-  const { config } = this;
   const themeCfg = this.theme.config;
+  const timezone = this.config.timezone || 'Asia/Shanghai';
 
-  const timezone = config.timezone || 'Asia/Shanghai';
-  console.log('timezone', timezone);
+  // console.log(data.date.format());
+  // console.log(data.date.tz(timezone).format());
 
   if(themeCfg.historyHash === false || data.historyHash === false) return data;
-  // User configuration
-  let shasum = crypto.createHash('sha1');
 
-  const frontMatterMatch = data.raw.match(/^---\n(.*?)\n---\n/s);
+  const obj = parseFrontMatter(data.raw);
+  if(obj === null)return;
 
-  if (frontMatterMatch === null) return data;
-
-  const frontMatterStr = frontMatterMatch[1];
-  const rawContentStr = data.raw.replace(/^---\n(.*?)\n---\n/s, '');
-
-  const obj = jsYaml.load(frontMatterStr, {
-    schema: jsYaml.JSON_SCHEMA
-  });
-
-  shasum.update(rawContentStr);
-  const currentTime = momentTZ().tz(timezone);
-  const shastr = shasum.digest('hex') + '#' + currentTime.format();
+  const rawContent = data.raw.replace(/^---\n(.*?)\n---\n/s, ''),
+    currentTime = moment().tz(timezone),
+    shastr = shaHash(rawContent) + '#' + currentTime.format();
 
   let flag = true;
   if (!obj.historyHash || obj.historyHash[0].replace(/#.*$/, '') !== shastr.replace(/#.*$/, '')) {
@@ -38,33 +41,36 @@ hexo.extend.filter.register('before_post_render', function (data) {
     if (!obj.historyHash) obj.historyHash = [shastr];
     else obj.historyHash.unshift(shastr);
   }
-  const latestTime = momentTZ(obj.historyHash[0].replace(/^.*?#/,''));
+  const latestTime = moment(obj.historyHash[0].replace(/^.*?#/,'')).tz(timezone);
   if(obj.date !== latestTime.tz(timezone).format()){
     flag = false;
     obj.date = latestTime.tz(timezone).format();
   }
   if(flag) {
-    console.log('latestTime', latestTime.format());
-    data.date = moment(latestTime.format('YYYY-MM-DD HH:mm:ss'));
+    // console.log('latestTime', latestTime.format());
+    data.date = latestTime.tz(timezone); //format('YYYY-MM-DD HH:mm:ss'));
     return data;
   }
 
   const newFrontMatterStr = jsYaml.dump(obj, { indent: 2 });
-
   const newRaw = data.raw.replace(/^---\n(.*?)\n---\n/s, () => `---\n${newFrontMatterStr}---\n`);
 
   fs.writeFileSync(data.full_source, newRaw, { encoding: 'utf8' });
 
   if (data.historyHash) data.historyHash.unshift(shastr);
   else data.historyHash = [shastr];
-  data.date = moment(currentTime.format('YYYY-MM-DD HH:mm:ss'));
+  data.date = currentTime.tz(timezone); //format('YYYY-MM-DD HH:mm:ss'));
+  data.raw = newRaw;
 
   return data;
 });
 
 hexo.extend.helper.register('historyParseTime', function (str) {
   const { timezone, language } = this.config;
-  return momentTZ(str.match(/#(.*?)$/)[1] || '').tz(timezone).locale(language).format('LL');
+  const date = moment(str.match(/#(.*?)$/)[1] || '');
+  log.info('Origin:          ', date.format()); 
+  log.info('Parsed Timezone: ', date.tz(timezone).format()); 
+  return date.tz(timezone).locale(language).format('LL');
 });
 
 hexo.extend.helper.register('historyParseHash', function (str) {
