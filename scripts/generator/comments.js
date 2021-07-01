7ad6cbc1483adfe54bd9aef8a3a7bb95f2305fb5
sshwy <jy.cat@qq.com>
2021-07-01 13:12:17 +0800

    fix: bug of comments page

--- scripts/generator/comments.js
+++ scripts/generator/comments.js
@@ -3,9 +3,11 @@
 hexo.extend.generator.register('comments', function () {
   const valine_paths = hexo.locals.get('posts').data.map(e => ({
     title: e.title,
-    valine_path: e.valinePath
+    valine_path: e.valinePath,
+    path: e.permalink,
   }));
-  console.log(valine_paths);
+  hexo.log.info('[hexo-theme-essence] generate comments page');
+  hexo.log.debug(valine_paths);
   return {
     path: 'comments/',
     data: {
